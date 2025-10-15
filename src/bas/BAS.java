a847283a3595cc60fd418bb7e60627d289069a17
Chapss <palmakarsten@gmail.com>
2025-10-16 07:23:18 +0800

    Flow of the program

--- src/bas/BAS.java
+++ src/bas/BAS.java
@@ -2,6 +2,25 @@ package bas;
 
 public class BAS {
 	public static void main(String[] args) {
-		//oks
+/*
+	 
+Flow of the bank simulation:
+1. Enter username and password
+
+2. Switch statement including:
+- Deposit
+- Withdraw
+- Balance
+- Exit
+
+3. Go back option when:
+- Depositing
+- Withdrawing
+- Balance
+
+4. This flow must be inside of a loop (do while maybe)
+		 
+		 
+*/
 	}
 }
