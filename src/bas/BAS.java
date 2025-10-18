6147c6b8d7a624d81c84fbe208062eb7f7cae738
Chapss <palmakarsten@gmail.com>
2025-10-18 22:40:38 +0800

    test recovering

---
+++ src/bas/BAS.java
@@ -0,0 +1,26 @@
+package bas;
+
+public class BAS {
+	public static void main(String[] args) {
+/*
+	 
+Flow of the bank simulation:
+1. Enter username and password
+
+2. Switch statement including:
+- Deposit
+- Withdraw
+- Balance
+- Exit
+
+3. Go back option when:
+- Depositing
+- Withdrawing
+- Balance
+
+4. This flow must be inside of a loop (do while maybe)
+		 
+		 
+*/
+	}
+}
